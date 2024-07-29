437a81560eba8f22672a06f32fcca81ec4a9a66f
alexwithstars <rayo080378@gmail.com>
2024-07-28 18:50:42 -0600

    fix(ShortInput): better portability

--- src/components/ShortInput.jsx
+++ src/components/ShortInput.jsx
@@ -1,17 +1,18 @@
-import { useEffect } from 'react'
+import { useEffect, useRef } from 'react'
 import './ShortInput.css'
 
-export function ShortInput ({ name, placeholder, initialValue = '', onChange }) {
+export function ShortInput ({ name, placeholder, data = '', onChange }) {
+  const input = useRef(null)
   useEffect(() => {
-    onChange('')
-  }, [])
+    input.current.value = data
+  }, [data])
   return (
     <label className='short-input'>
       <span className='short-input_name'>{name}</span>
       <input
+        ref={input}
         className='short-input_input'
         type='text'
-        defaultValue={initialValue}
         placeholder={placeholder || name}
         onChange={e => {
           onChange(e.target.value)
