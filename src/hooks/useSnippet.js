f6bb85830f3cce31f8afd11dfc14bfea5fff5458
alexwithstars <rayo080378@gmail.com>
2024-07-28 18:18:08 -0600

    fix(useSnippet): tabs support

--- src/hooks/useSnippet.js
+++ src/hooks/useSnippet.js
@@ -1,17 +1,24 @@
 import { useState, useEffect } from 'react'
 
-export function useSnippet ({ name, prefix, description, code }) {
+export function useSnippet ({
+  name = '',
+  prefix = '',
+  description = '',
+  code = '',
+  tabSize = 2
+}) {
   const [snippet, setSnippet] = useState('')
   const specialCharacters = /["'\\]/g
   const htmlg = />/g
   const htmll = /</g
   const toSnippet = /^.*$/gm
+  const tabs = new RegExp(`\t| {${tabSize}}`, 'g')
   const getSnippet = () => {
     const formatedCode = (code || '')
       .replace(specialCharacters, match => `\\${match}`)
       .replace(htmlg, '&gt;')
       .replace(htmll, '&lt;')
-      .replace(/\t| {2}/g, '\\t')
+      .replace(tabs, '\\t')
       .replace(toSnippet, match => `\t\t"${match}",`)
       .slice(0, -1)
     const resultSnippet =
