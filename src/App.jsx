2bc0b7559c62e77ba7cea068b196880e8afd85cd
alexwithstars <rayo080378@gmail.com>
2024-07-28 18:54:44 -0600

    feat(SideMenu): implementation of side menu

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from 'react'
 import { Bars3Icon, ClipboardDocumentIcon, BookmarkIcon } from '@heroicons/react/24/outline'
 import { useSnippet } from './hooks/useSnippet'
 import { useToast } from './hooks/useToast'
+import { useStorage } from './hooks/useStorage'
 import { ShortInput } from './components/ShortInput'
 import { CodeEditor } from './components/CodeEditor'
 import { ToastList } from './components/ToastList'
@@ -12,9 +13,34 @@ import { messages } from './scripts/messages'
 import { SideMenu } from './components/SideMenu'
 
 export default function App () {
-  const [fields, setFields] = useState({})
+  const defaultFields = {
+    id: 0,
+    name: '',
+    prefix: '',
+    description: '',
+    code: ''
+  }
+  const [fields, setFields] = useState(() => {
+    const sessionData = window.sessionStorage.getItem('sessionData')
+    try {
+      return sessionData ? JSON.parse(sessionData) : defaultFields
+    } catch (error) { return defaultFields }
+  })
   const { toasts, addToast, removeToast } = useToast()
-  const snippet = useSnippet(fields)
+  const [settings, setSettings] = useState(() => {
+    const setting = window.localStorage.getItem('settings')
+    const defaultSettings = {
+      preview: true,
+      tabs: false,
+      fontSize: 18
+    }
+    try {
+      return setting ? JSON.parse(setting) : defaultSettings
+    } catch (error) { return defaultSettings }
+  })
+  const { snippets, addSnippet, removeSnippet } = useStorage()
+  const [showSideMenu, setShowSideMenu] = useState(false)
+  const snippet = useSnippet({ ...fields, tabSize: settings.tabs ? 4 : 2 })
   const inputs = {
     name: 'My snippet',
     prefix: 'snippet',
@@ -22,20 +48,20 @@ export default function App () {
   }
 
   useEffect(() => {
-    const sessionData = window.sessionStorage.getItem('sessionData')
-    if (sessionData) {
-      setFields(JSON.parse(sessionData))
-    }
-  }, [])
-  useEffect(() => {
     window.sessionStorage.setItem('sessionData', JSON.stringify(fields))
-  }, [snippet])
+  }, [fields])
+  useEffect(() => {
+    window.localStorage.setItem('settings', JSON.stringify(settings))
+  }, [settings])
 
   return (
     <>
       <header className='header'>
         <h1 className='header-title'>vSnippet</h1>
-        <Bars3Icon className='header-icon' onClick={() => addToast(messages.development)} />
+        <Bars3Icon
+          className='header-icon'
+          onClick={() => setShowSideMenu(true)}
+        />
       </header>
       <main className='main-content'>
         <section className='short-inputs'>
@@ -44,28 +70,30 @@ export default function App () {
               key={inputName}
               name={inputName}
               placeholder={inputPlaceholder}
-              initialValue={fields[inputName] || ''}
+              data={fields[inputName] || ''}
               onChange={value => {
                 setFields((prev) => ({ ...prev, [inputName]: value }))
               }}
             />
           ))}
         </section>
-        <section className='editors'>
+        <section className='editors' style={{ tabSize: settings.tabs ? 4 : 2 }}>
           <CodeEditor
             editable
             name='code'
             placeholder='console.log("Hello World")'
-            initialValue={fields.code || ''}
+            data={fields.code || ''}
             onChange={value => {
               setFields((prev) => ({ ...prev, code: value }))
             }}
+            sizeFont={settings.fontSize}
           />
-          <CodeEditor
-            name='preview'
-            value={snippet}
-            onChange={() => { console.log('changed') }}
-          />
+          {settings.preview &&
+            <CodeEditor
+              name='preview'
+              data={snippet}
+              sizeFont={settings.fontSize}
+            />}
         </section>
       </main>
       <div className='actions'>
@@ -73,20 +101,44 @@ export default function App () {
           text='Save'
           Icon={BookmarkIcon}
           onClick={() => {
-            addToast(messages.development)
+            addSnippet(fields)
+            addToast(messages.snippetSaved)
           }}
         />
         <PrimaryButton
           text='Copy'
           Icon={ClipboardDocumentIcon}
           onClick={() => {
-            navigator.clipboard.writeText(snippet)
+            window.navigator.clipboard.writeText(snippet)
               .then(() => addToast(messages.copiedToClipboard))
               .catch(() => addToast(messages.canNotCopyToClipboard))
           }}
         />
       </div>
-      <SideMenu />
+      {showSideMenu &&
+        <SideMenu
+          onClose={() => setShowSideMenu(false)}
+          onChangeSettings={setSettings}
+          settings={settings}
+          snippets={snippets}
+          onRemoveSnippet={(id) => {
+            removeSnippet(id)
+            addToast(messages.snippetDeleted)
+          }}
+          onSetSnippet={(fields) => {
+            setFields(fields)
+            addToast(messages.snippetSetAsActive)
+          }}
+          onFirstSnippet={() => {
+            addSnippet(fields)
+            addToast(messages.snippetSaved)
+          }}
+          onCopySnippet={(snippet) => {
+            window.navigator.clipboard.writeText(snippet)
+              .then(() => addToast(messages.copiedToClipboard))
+              .catch(() => addToast(messages.canNotCopyToClipboard))
+          }}
+        />}
       <ToastList removeToast={removeToast} toasts={toasts} />
     </>
   )
