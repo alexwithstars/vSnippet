75895b160f5216d7a4f1567256920d903fefd275
alexwithstars <rayo080378@gmail.com>
2024-07-29 20:25:06 -0600

    fix(robots): new location

--- vite.config.js
+++ vite.config.js
@@ -11,8 +11,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
-        colors: resolve(__dirname, 'colors.html'),
-        robots: resolve(__dirname, 'robots.txt')
+        colors: resolve(__dirname, 'colors.html')
       }
     }
   }
