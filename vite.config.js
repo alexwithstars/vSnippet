7954bfaefde55f9225e8b0418bb81f679ac9dfcf
alexwithstars <rayo080378@gmail.com>
2024-07-29 20:17:21 -0600

    feat(robots): robots.txt file

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,8 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
-        colors: resolve(__dirname, 'colors.html')
+        colors: resolve(__dirname, 'colors.html'),
+        robots: resolve(__dirname, 'robots.txt')
       }
     }
   }
